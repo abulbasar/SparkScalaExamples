7ef1f3805128c156fc03d6782b8efa194a285d37
Abul Basar <abulbasar@users.noreply.github.com>
2017-07-16 13:40:29 +0530

    Update StreamApp.scala

--- src/main/scala/StreamApp.scala
+++ src/main/scala/StreamApp.scala
@@ -5,16 +5,29 @@ import org.apache.spark.storage.StorageLevel
 
 
 object StreamApp {
+  
+   
   def main(args:Array[String]) {
+    
     val conf = new SparkConf()
     .setAppName(getClass.getName)
     .setIfMissing("spark.master", "local[*]")
     
     val sc = new SparkContext(conf)
     
-    val ssc = new StreamingContext(sc, Seconds(5))
+    val checkpointDir = "spark-checkpoint"
+    val rawStorage = "raw-storage"
+    
+    def createSSC() = {
+      val ssc =  new StreamingContext(sc, Seconds(5))
+      ssc.checkpoint(checkpointDir)
+      ssc
+    }
+    
+    val ssc = StreamingContext.getOrCreate(checkpointDir, createSSC)
     
     val raw = ssc.socketTextStream("localhost", 9999, StorageLevel.MEMORY_ONLY)
+    raw.saveAsTextFiles(rawStorage + "/stream", "")
     
     raw.print()
     
