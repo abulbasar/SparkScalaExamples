42cff3d1fae67450021a34c2bccfbe7dc4bd9d2f
Abul Basar <abulbasar@users.noreply.github.com>
2017-07-16 13:13:25 +0530

    Update WordCount.scala

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -2,19 +2,29 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 
 object WordCount {
+  
   def main(args: Array[String]): Unit = {
-    val path = "/etc/passwd"
+    
+    if(args.size == 0){
+      println("Usage: WordCount <input path>")
+      System.exit(0)
+    }
+    
+    val path = args(0)
+    print(s"Input path: $path")
+    
     val conf = new SparkConf()
-
-    conf.setAppName("sample demo")
-    conf.setMaster("local[*]")
+      .setAppName(getClass.getName)
+      .setIfMissing("spark.master", "local")
+      
     val sc = new SparkContext(conf)
-    sc.textFile(path).
-      flatMap(_.split("\\W+")).
-      filter(_.length > 0).
-      map((_, 1)).
-      reduceByKey(_+_).
-      collect.
-      foreach(println)
+    
+    sc.textFile(path)
+      .flatMap(_.toLowerCase.split("\\W+"))
+      .filter(_.length > 0)
+      .map((_, 1))
+      .reduceByKey(_+_)
+      .collect
+      .foreach(println)
   }
 }
