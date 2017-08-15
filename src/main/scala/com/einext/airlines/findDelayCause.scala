4bc1aad19c180b9bf24df633fc93ef46ca4f89c0
Abul Basar <abulbasar@users.noreply.github.com>
2017-08-16 02:07:44 +0530

    Create findDelayCause.scala

---
+++ src/main/scala/com/einext/airlines/findDelayCause.scala
@@ -0,0 +1,31 @@
+package com.einext.airlines
+
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.api.java.UDF1
+
+/*
+* Usage: find cause of airline delay based on 5 fields. Find the field name which has non-zero highest value.
+* Use this UDF for faster execution on larger dataset.
+*
+* sqlContext.registerJavaFunction("findDelayCause", "com.einext.airlines.findDelayCause")
+* data.selectExpr("findDelayCause(struct(*))").show()
+*
+* */
+
+class findDelayCause extends UDF1[Row, String] {
+
+  @throws[Exception]
+  override def call(row: Row): String = {
+    val fields: Array[String] = "CarrierDelay,WeatherDelay,NASDelay,SecurityDelay,LateAircraftDelay".split(",").map(_.trim)
+    val values = fields.map{field =>
+      val v: String = row.getString(row.fieldIndex(field))
+      if (v != null && v.trim.length > 0) v.trim.toDouble else -1.0
+    }
+    var cause:String = null
+    if(values.max > 0){
+      val i = values.indexOf(values.max)
+      cause = fields(i)
+    }
+    cause
+  }
+}
