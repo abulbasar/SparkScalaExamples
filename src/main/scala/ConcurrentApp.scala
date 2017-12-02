5c42145e28c8a1bb44e4b975c7e63d6e6256cf6f
Abul Basar <abulbasar@users.noreply.github.com>
2017-12-03 02:14:39 +0530

    Update ConcurrentApp.scala

--- src/main/scala/ConcurrentApp.scala
+++ src/main/scala/ConcurrentApp.scala
@@ -1,33 +1,47 @@
 import org.apache.spark.sql.SparkSession
-import import java.util.concurrent.Executors
+import java.util.concurrent.Executors
 import scala.concurrent._
 import scala.concurrent.duration._
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.SparkConf
 
-object ConcurrentApp {
-  def def appMain(args: Array[String]) = {
+
+object MyApp {
+  def main(args: Array[String]){
+    
+    val conf = new SparkConf()
+    .setAppName(getClass.getName)
+    .setIfMissing("spark.master", "local[*]")
 
     val spark = SparkSession
         .builder
-        .appName(getClass.getName)
+        .config(conf)
         .getOrCreate()
         
     val pool = Executors.newFixedThreadPool(5)
     
     implicit val xc = ExecutionContext.fromExecutorService(pool)
     
-    val df = spark.sparkContext.parallelize(1 to 100).toDF
+    val df = spark.range(100).toDF("id")
+    
+    
+    
     val tasks = Seq(
-      taskA(df, "hdfs:///dis.parquet"),
-      taskB(df, "hdfs:///dis.parquet")
+      taskA(df, "out1"),
+      taskB(df, "out2")
     )
     Await.result(Future.sequence(tasks), Duration(1, MINUTES))
+    spark.close()
+    pool.shutdown()
   }
 
-  def taskA(df: DataFrame, outPath: String)(implicit xc: ExecutionContext) = Future {
-    df.distinct.write.parquet(outPath)
+  def taskA(df: Dataset[Row], outPath: String)(implicit xc: ExecutionContext) = Future {
+    df.distinct.write.mode(SaveMode.Overwrite).save(outPath)
   }
 
-  def taskB(df: DataFrame, outPath: String)(implicit xc: ExecutionContext) = Future {
-    df.agg(sum("value")).write.parquet(outPath) 
+  def taskB(df: Dataset[Row], outPath: String)(implicit xc: ExecutionContext) = Future {
+    df.distinct.write.mode(SaveMode.Overwrite).save(outPath)
   }
 }
